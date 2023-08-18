a5d90eab83a3044298a4c099506f0a82d734cf6d
shohaib1996 <khanshohaibhossain@gmail.com>
2023-08-18 12:58:56 +0600

    added convert

--- js/area-calculator.js
+++ js/area-calculator.js
@@ -9,8 +9,13 @@ function calculateTriangleArea() {
     const height = parseFloat(heightValueText);
     heightField.value = '';
     const area = 0.5 * base * height;
+    if(isNaN(base) || isNaN(height)){
+        alert('Please provide a number');
+        return;
+    }
     const areaSpan = document.getElementById('triangle-area');
     areaSpan.innerText = area;
+    addToCalculationEntry('Triangle', area)
 
 }
 function calculateRectangleArea(){
@@ -27,13 +32,19 @@ function calculateRectangleArea(){
     const area = width * length;
     const areaSpan = document.getElementById('rectangle-area');
     areaSpan.innerText = area;
+    addToCalculationEntry('rectangle', area);
 
 }
 function calculateParallelogramArea(){
     const base = getInputValue('parallelogram-base');
     const height = getInputValue('parallelogram-height');
+    if(isNaN(base) || isNaN(height)){
+        alert('Please provide a Number');
+        return;
+    }
     const area = base * height;
     setElementInnerText('parallelogram-area', area);
+    addToCalculationEntry('parallelogram', area);
 
 }
 
@@ -41,7 +52,8 @@ function calculateEllipseArea(){
     const majorRadius = getInputValue('major-radius');
     const minorRadius = getInputValue('minor-radius');
     const area = (3.14 * majorRadius * minorRadius).toFixed(2);
-    setElementInnerText('ellipse-area', area)
+    setElementInnerText('ellipse-area', area);
+    addToCalculationEntry('Ellipse', area)
 }
 
 function getInputValue(fieldId){
@@ -54,5 +66,16 @@ function getInputValue(fieldId){
 function setElementInnerText(elementId, area){
     const element = document.getElementById(elementId);
     element.innerText = area;
+
+}
+
+function addToCalculationEntry(areaType, area){
+    const calculationEntry = document.getElementById('calculation-entry');
+    const count = calculationEntry.childElementCount;
+    const p = document.createElement('p');
+    p.classList.add('text-center');
+    p.classList.add('my-5');
+    p.innerHTML = `${count + 1}.   ${areaType}   ${area}  cm<sup>2</sup>  <button class="btn btn-sm btn-success">Convert</button>`;
+    calculationEntry.appendChild(p)
 
 }
